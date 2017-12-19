abc0a336eb62d551c5714e68cea6c070eb7c00f9
vitusignacio <vitus.ignacio@gmail.com>
2017-12-20 00:00:15 +0700

    updated files

--- app.js
+++ app.js
@@ -4,23 +4,39 @@ var plugins = require('./plugins');
 
 var bot_token = 'xoxb-287887410179-4BnqFtyiiZqB3fuOqt1EvjwD';
 var rtm = new plugins.RtmClient(bot_token);
+var token = 'xoxp-2496922627-281226671255-287713637027-5e2dfa96bf00e27c213dd9905f9bc221';
+var web = new plugins.WebClient(token);
 var sourceChannelId, destChannelId;
 var sourceChannelName, destChannelName;
 var listOfChannels = [];
 
+defaultLanguage = process.env.DESTINATION_LANGUAGE || 'en';
+defaultEnvironment = process.env.ENVIRONMENT || 'dev';
+
+console.log('Specified language: ' + defaultLanguage);
+console.log('Specified environment: ' + defaultEnvironment);
+
 rtm.on(plugins.CLIENT_EVENTS.RTM.AUTHENTICATED, function(rtmStartData) {
+    var observableChannel = null;
+    if (defaultEnvironment != 'dev' &&
+        defaultEnvironment != 'test')
+    {
+        observableChannel = 'pvt_translator_' + defaultLanguage;
+    } else
+    {
+        observableChannel = 'pvt_translator_' + defaultEnvironment;
+    }
+    
+    console.log('Effective channel: ' + observableChannel);
     for (var i = 0; i < rtmStartData.channels.length; i++) {
         if (rtmStartData.channels[i].is_member) {
-            if (rtmStartData.channels[i].name == 'pvt_translator') {
+            if (rtmStartData.channels[i].name == observableChannel) {
                 destChannelId = rtmStartData.channels[i].id;
                 destChannelName = rtmStartData.channels[i].name;
             }
-            listOfChannels.push(rtmStartData.channels[i]);
         }
+        listOfChannels.push(rtmStartData.channels[i]);
     }
-    
-    console.log('Determining destination... ' + destChannelId);
-    console.log('Logged in but not yet connected to a channel');
 });
 
 rtm.on(plugins.CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function() {});
@@ -29,10 +45,10 @@ rtm.on(plugins.RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
     var messageChannelId = message.channel;
     var messageChannelName = new plugins.LINQ(listOfChannels).Where(function(c) { return c.id == message.channel; }).ToArray()[0].name;
     if (messageChannelId != destChannelId) {
-        plugins.Translate(message.text, { from: 'ja', to: 'en' }).then(function(output) {
-            console.log("Pushing...");
-            rtm.sendMessage('[' + messageChannelName + '] ' + output.text, destChannelId);
-            console.log("Pushed");
+        plugins.Translate(message.text, { from: 'ja', to: defaultLanguage }).then(function(output) {
+            web.users.info(message.user, function (err, info) {
+                rtm.sendMessage('*' + info.user.real_name + '* *' + messageChannelName + '* ' + output.text, destChannelId);
+            });
         });
     }
 });
--- plugins.js
+++ plugins.js
@@ -1,6 +1,7 @@
 module.exports = {
     Translate: require('google-translate-api'),
     RtmClient: require('@slack/client').RtmClient,
+    WebClient: require("@slack/client").WebClient,
     CLIENT_EVENTS: require('@slack/client').CLIENT_EVENTS,
     RTM_EVENTS: require('@slack/client').RTM_EVENTS,
     LINQ: require('node-linq').LINQ
